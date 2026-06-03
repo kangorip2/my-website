975e13bea85fafd7f55983c4ba8401089e6e1c7f
kangorip2 <kangorip2@gmail.com>
2026-06-03 23:08:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+let images = [];
+let currentIndex = 0;
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    const imgElements = document.querySelectorAll(".gallery-img");
+
+    imgElements.forEach((img, index) => {
+        images.push(img.src);
+
+        img.addEventListener("click", () => {
+            openLightbox(index);
+        });
+    });
+
+});
+
+// פתיחת תמונה
+function openLightbox(index){
+    currentIndex = index;
+    document.getElementById("lightbox-img").src = images[index];
+    document.getElementById("lightbox").style.display = "flex";
+}
+
+// סגירה
+function closeLightbox(){
+    document.getElementById("lightbox").style.display = "none";
+}
+
+// מעבר בין תמונות
+function changeImage(direction){
+    currentIndex += direction;
+
+    if(currentIndex < 0){
+        currentIndex = images.length - 1;
+    }
+
+    if(currentIndex >= images.length){
+        currentIndex = 0;
+    }
+
+    document.getElementById("lightbox-img").src = images[currentIndex];
+}
